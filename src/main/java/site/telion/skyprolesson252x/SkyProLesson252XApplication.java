c9ae2e6fef18280290573bd0ad0b8352e8c25c0f
Telion <www.neon@mail.ru>
2022-10-01 12:31:51 +0500

    Initial commit

---
+++ src/main/java/site/telion/skyprolesson252x/SkyProLesson252XApplication.java
@@ -0,0 +1,13 @@
+package site.telion.skyprolesson252x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProLesson252XApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProLesson252XApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/telion/skyprolesson252x/SkyProLesson252XApplicationTests.java
@@ -0,0 +1,13 @@
+package site.telion.skyprolesson252x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProLesson252XApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
